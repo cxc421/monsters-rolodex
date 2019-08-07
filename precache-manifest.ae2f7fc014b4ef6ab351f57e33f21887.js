442505f147e664e13290b76fce7c3714978c4a0e
Chris Chuang <chrischuang0421@gmail.com>
2019-08-07 14:34:58 +0800

    Updates

---
+++ precache-manifest.ae2f7fc014b4ef6ab351f57e33f21887.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b5b789326140ffdc0464cc186641515",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9a649fbba640b27f5f7c",
+    "url": "/monsters-rolodex/static/css/main.b5f2bee8.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "9a649fbba640b27f5f7c",
+    "url": "/monsters-rolodex/static/js/main.00032315.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
